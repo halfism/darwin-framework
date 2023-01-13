09fdfa4855b22c4f8d47a4aa61bd061c01b0dbbf
huxiaomin <1072963235@qq.com>
2023-01-14 00:47:48 +0800

    test mvn deploy

---
+++ src/main/java/com/halfism/darwinframework/DarwinFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.halfism.darwinframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarwinFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarwinFrameworkApplication.class, args);
+	}
+
+}
